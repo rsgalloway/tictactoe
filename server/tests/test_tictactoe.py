be92ce0f1fb75376a132f9b90d82634fd1603462
Ryan Galloway <ryan@rsg.io>
2025-09-04 06:19:05 -0700

    initial game logic tests

--- server/tests/test_tictactoe.py
+++ server/tests/test_tictactoe.py
@@ -1,3 +1,23 @@
 __doc__ = """
 Contains the tests for the tictactoe.py module.
 """
+
+from tictactoe import is_terminal, new_board
+
+
+def test_new_board_empty():
+    assert new_board() == "........."
+
+
+def test_terminal_detection():
+    b = "XXX......"
+    status, line = is_terminal(b)
+    assert status == "x_won" and set(line) == {0, 1, 2}
+
+    b = "OOO......"
+    status, line = is_terminal(b)
+    assert status == "o_won" and set(line) == {0, 1, 2}
+
+    b = "XOXOOXXXO"
+    status, _ = is_terminal(b)
+    assert status in {"draw", "x_won", "o_won"}
